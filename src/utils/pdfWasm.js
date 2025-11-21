81450bab418f3e8ae89bd9f16c16bed4c8b144b0
vosej565 <vosej565@users.noreply.github.com>
2025-11-21 14:10:46 +0900

    qqq43

--- src/utils/pdfWasm.js
+++ src/utils/pdfWasm.js
@@ -1,17 +1,22 @@
-export async function _GSPS2PDF( dataStruct,
-                                   responseCallback,
-                                   progressCallback,
-                                   statusUpdateCallback){
-  const worker = new Worker(new URL("/gs/worker.js", import.meta.url));
+export async function _GSPS2PDF(
+  dataStruct,
+  responseCallback,
+  progressCallback,
+  statusUpdateCallback
+) {
 
-  worker.postMessage({ data: dataStruct, target: 'wasm'});
-  return new Promise((resolve, reject)=>{
+  const worker = new Worker(
+    new URL('/gs/worker.js', import.meta.url)   // ← ★ 이거 하나만 바꾸면 됨
+  );
+
+  worker.postMessage({ data: dataStruct, target: 'wasm' });
+
+  return new Promise((resolve, reject) => {
     const listener = (e) => {
-      resolve(e.data)
-      worker.removeEventListener('message', listener)
-      setTimeout(()=> worker.terminate(), 0);
-    }
+      resolve(e.data);
+      worker.removeEventListener('message', listener);
+      setTimeout(() => worker.terminate(), 0);
+    };
     worker.addEventListener('message', listener);
-  })
-
+  });
 }
